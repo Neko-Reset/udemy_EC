3b689e7082ea5bc0a25f6c057eea69e1adbee649
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2023-02-18 23:16:53 +0900

    ライフサイクルテスト

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest() {
+        // サービス登録
+        app()->bind("lifeCycleTest", function(){
+            return "ライフサークル";
+        });
+
+        // サービス取り出し
+        $test = app()->make("lifeCycleTest");
+
+        // サービスなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        // サービスコンテナapp()ありのパターン
+        // newでインスタンス化しなくても使える
+        app()->bind("sample", Sample::class);
+        $sample = app()->make("sample");
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class sample{
+    public $message;
+    public function __construct(Message $message) {
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send(){
+        echo("メッセージ");
+    }
+}
